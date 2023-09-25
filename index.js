b7fc0f39bc1d9590e36fbc2d7742a6e93a4a532e
ENZINHO's PIZZARIA <108340007+EnzoXor@users.noreply.github.com>
2023-09-25 16:10:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-function toggle(){
-    const calendar = document.querySelectorAll(".botomb .calendar")
-   
+var tela = 'casa'
 
-    calendar.classList.toggle("active")
+
+
+    const agenda = document.querySelector(".agenda")
+    if (agenda.classList.contains('invisible'), tela =='agenda'){
+
+        agenda.classList.remove('invisible')
+    }
+    else{
+        agenda.classList.add('invisible')
+    }
+
+
+
+    const login = document.querySelector(".login")
+    if (login.classList.contains('invisible'), tela =='login'){
+
+        login.classList.remove('invisible')
+    }else{
+        login.classList.add('invisible')
+    }
+
+    const casa = document.querySelector(".casa")
+    if (casa.classList.contains('invisible'), tela =='casa'){
+
+        casa.classList.remove('invisible')
+    }else{
+        casa.classList.add('invisible')
+    }
+
+
+function change(){
+    const buttons = document.querySelectorAll(".botomb button")
+    buttons.classList.toggle("active")
+    
+}
+function color(){
+    const buttons = document.documentElement
+    buttons.classList.toggle("dark")
 }
